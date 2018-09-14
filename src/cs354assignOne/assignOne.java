4781a48c27d9b1f8d533354816cddca069a0cb1f
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-09-13 22:47:40 -0500

    adding switch calculator

--- src/cs354assignOne/assignOne.java
+++ src/cs354assignOne/assignOne.java
@@ -1,10 +1,33 @@
 package cs354assignOne;
 
+import java.util.Scanner;
+
 public class assignOne {
 
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		String ans = "";
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What math operator do you want to perform?");
+		ans = input.nextLine();
+		
+		switch(ans) {
+
+		case "+": 
+			System.out.println("Add");
+			break;
+		case "-":
+			System.out.println("Sub ");
+			break;
+		case "/":
+			System.out.println("Div");
+			break;
+		case "*":
+		default: 
+			System.out.println("Fuck you");
+			break;
 	}
+}
 }
