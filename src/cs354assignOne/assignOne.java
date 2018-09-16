18fb70f071e224cc21a4df74475b4784bc90414b
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-09-15 20:50:36 -0500

    completing calculator

--- src/cs354assignOne/assignOne.java
+++ src/cs354assignOne/assignOne.java
@@ -1,30 +1,51 @@
 package cs354assignOne;
 
 import java.util.Scanner;
+import java.util.Random;
+
+
 
 public class assignOne {
 
 	
 	public static void main(String[] args) {
 		
+		Random rand = new Random();
+		Scanner input = new Scanner(System.in);
+
+		int  num1 = rand.nextInt(100) + 1;
+		int  num2 = rand.nextInt(100) + 1;
+	
 		String ans = "";
 		
-		Scanner input = new Scanner(System.in);
 		System.out.println("What math operator do you want to perform?");
 		ans = input.nextLine();
+		
+		System.out.println("The first random number is: " + num1);
+		System.out.println("The second random number is: " + num2);
 		
 		switch(ans) {
 
 		case "+": 
 			System.out.println("Add");
+			int addAns = num1 + num2;
+			System.out.println(addAns);
 			break;
 		case "-":
 			System.out.println("Sub ");
+			int subAns = num1 - num2;
+			System.out.println(subAns);
 			break;
 		case "/":
 			System.out.println("Div");
+			int divAns = num1/num2;
+			System.out.println(divAns);
 			break;
 		case "*":
+			System.out.println("Multi");
+			int multiAns = num1*num2;
+			System.out.println(multiAns);
+			break;
 		default: 
 			System.out.println("Fuck you");
 			break;
