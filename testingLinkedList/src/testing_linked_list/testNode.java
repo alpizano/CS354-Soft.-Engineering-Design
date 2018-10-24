0d5cd1ee77a58bb66deb640910caf7706f174c83
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-10-23 21:07:18 -0500

    testing algorithms BST, dynamic implemented via linked list

---
+++ testingLinkedList/src/testing_linked_list/testNode.java
@@ -0,0 +1,26 @@
+package testing_linked_list;
+
+public class testNode {
+	testNode head;
+	testNode tail;
+	int data;
+	
+	public testNode() {
+		head = new testNode();
+		tail = new testNode();
+	}
+	
+	public testNode(int data) {
+		head =
+	}
+	
+	public testNode(int num, testNode node) {
+		head = 
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
